02129a120a70c46777f62157f8a32a3da279a568
vineet <jammackp@gmail.com>
2025-11-30 19:43:31 +0530

    first commit

---
+++ javascript/basics/01-polyfills.js
@@ -0,0 +1,5 @@
+// What are polyfills?
+/* -------------------------- */
+/* Polyfills are pieces of code (usually JavaScript) used to provide modern functionality on older browsers that do not natively support it. They "fill in" the gaps by implementing features that are missing in those environments.
+*/
+
