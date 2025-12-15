026c4c55a58dd16d66aed59e50dc3079f708326d
vineet <jammackp@gmail.com>
2025-12-15 22:05:08 +0530

    adding typescript

--- javascript/basics/01-polyfills.js
+++ javascript/basics/01-polyfills.js
@@ -25,6 +25,38 @@ function myFlat(arr, depth = 1, output = []) {
 
 console.log(myFlat(arr, 4)); // [1, 2, 3, 4, 5, 6, 4, 3, 5, 1, 2, 3]
 
+//Polyfill for Array.prototype.filter
+
+if (!Array.prototype.myFilter) {
+  Array.prototype.myFilter = function(callback, thisArg) {
+    if (this == null) {
+      throw new TypeError('Array.prototype.myFilter called on null or undefined');
+    }
+    if (typeof callback !== 'function') {
+      throw new TypeError(callback + ' is not a function');
+    }
+
+    const result = [];
+    const array = Object(this);
+    const len = array.length >>> 0;
+
+    for (let i = 0; i < len; i++) {
+      if (i in array) {
+        const element = array[i];
+        if (callback.call(thisArg, element, i, array)) {
+          result.push(element);
+        }
+      }
+    }
+
+    return result;
+  };
+}
+
+// Example usage of the polyfill
+const numbers = [1, 2, 3, 4, 5];
+const evenNumbers = numbers.myFilter(num => num % 2 === 0
+
 // Transpilers
 /* -------------------------- */
 /* Transpilers are tools that convert code written in one programming language or version into another. In the context of JavaScript, transpilers like Babel convert modern JavaScript (ES6 and beyond) into older versions (like ES5) to ensure compatibility with older browsers.
